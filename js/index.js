acb5ea6532755b7c0703f257738ac7d346863642
Daniel Lubovich <daniellubovich@u.boisestate.edu>
2016-11-08 19:23:14 -0700

    Added a basic content script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var runButton = document.getElementById('runButton');
+    runButton.addEventListener('click', run);
+});
 
+function run() {
+    console.log('running');
+}
